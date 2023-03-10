cc3bdd4bacd0d17a32d7aa63e06b54201c8427ec
sofiaj2 <sej59@scarletmail.rutgers.edu>
2023-03-09 20:15:23 -0500

    thursday mar 9

--- studentInterfaceGUI/src/main/java/com/example/studentinterfacegui/HelloApplication.java
+++ studentInterfaceGUI/src/main/java/com/example/studentinterfacegui/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 1500, 1500);
         stage.setTitle("Tuition Manager Graphical User Interface");
         stage.setScene(scene);
         stage.show();
