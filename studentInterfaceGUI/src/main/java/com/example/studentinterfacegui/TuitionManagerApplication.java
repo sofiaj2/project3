b27a5be68a6e7853219383a765ead7cb25b0bc1a
sofiaj2 <sej59@scarletmail.rutgers.edu>
2023-03-17 22:12:05 -0400

    mar 17 at 10pm

--- studentInterfaceGUI/src/main/java/com/example/studentinterfacegui/TuitionManagerApplication.java
+++ studentInterfaceGUI/src/main/java/com/example/studentinterfacegui/TuitionManagerApplication.java
@@ -14,7 +14,7 @@ public class TuitionManagerApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(TuitionManagerApplication.class.getResource("TuitionManagerView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
         stage.setTitle("Tuition Manager Graphical User Interface");
         stage.setScene(scene);
         stage.show();
